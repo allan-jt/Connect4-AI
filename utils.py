30309d079458c8a4f14d098305f1f87d22e360ea
athekkep <athekkep@student.42abudhabi.ae>
2022-11-07 16:55:39 +0400

    clean up

--- utils.py
+++
@@ -1,7 +0,0 @@
-from typing import Union
-
-list_tuple = Union[tuple, list]
-
-def center_coordinate(screen_hw:list_tuple, shape_hw:list_tuple) -> list_tuple:
-	return (screen_hw[0]/2 - shape_hw[0]/2, screen_hw[1]/2 - shape_hw[1]/2)	
-	
